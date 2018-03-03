e059466d08bcd548ea9b4d4d2a6ebb472cd8b995
Artem <artem2325@bk.ru>
2018-03-03 06:14:08 +0300

    Minmax exercise fixed

--- Minmax.java
+++ Minmax.java
@@ -1,27 +1,31 @@
-import java.util.*;
+import java.util.Arrays;
 
-public class minmax {
-    public static void main(String[] Args) {
-        int[] newArray = {1, 5, 4825, 10, 34, -5, 54, 11, 65, 0};
-        int lowestNumber = 0;
-        int highestNumber = 0;
-        System.out.println("Array input: " + Arrays.toString(newArray));
-        for (int i = 0; i < newArray.length; i++) {
-            if (newArray[i] > highestNumber) {
-                highestNumber = newArray[i];
+public class Minmax {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 4825, 10, 34, -5, 54, 11, 65, 0};
+        int lowest = 0;
+        int highest = 0;
+        int lowestCell = 0;
+        int highestCell = 0;
+        System.out.println("Array input: " + Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > highest) {
+                highest = array[i];
+                highestCell = i;
             }
         }
-        for (int i = 0; i < newArray.length; i++) {
-            if (newArray[i] < lowestNumber) {
-                lowestNumber = newArray[i];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < lowest) {
+                lowest = array[i];
+                lowestCell = i;
             }
         }
-        System.out.println("Highest number: " + highestNumber);
-        System.out.println("Lowest number: " + lowestNumber);
-        lowestNumber = 0;
-        highestNumber = 99;
-        newArray[1] = lowestNumber;
-        newArray[6] = highestNumber;
-        System.out.println("Array output: " + Arrays.toString(newArray));
+        System.out.println("Highest number: " + highest);
+        System.out.println("Lowest number: " + lowest);
+        lowest = 0;
+        highest = 99;
+        array[lowestCell] = lowest;
+        array[highestCell] = highest;
+        System.out.println("Array output: " + Arrays.toString(array));
     }
 }
